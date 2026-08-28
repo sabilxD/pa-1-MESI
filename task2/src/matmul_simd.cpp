31ea13c12d9e70d0467943082399dd275709cb95
Abhineet Majety <abhineetm13@users.noreply.github.com>
2026-08-28 17:58:31 +0530

    updated simd to single precision (it was previously double precision). correctness: yes for multiples of 8. speedup~2.2x

--- task2/src/matmul_simd.cpp
+++ task2/src/matmul_simd.cpp
@@ -13,30 +13,36 @@ void matmul_simd(const float* A, const float* B, float* C,
                              A + static_cast<long>(i+1) * lda,
                              A + static_cast<long>(i+2) * lda,
                              A + static_cast<long>(i+3) * lda};
-        for (int j = 0; j < N; j+=4) {
+        for (int j = 0; j < N; j+=8) {
             const float* b0 = B + static_cast<long>(j) * ldb;
             const float* b1 = B + static_cast<long>(j+1) * ldb;
             const float* b2 = B + static_cast<long>(j+2) * ldb;
             const float* b3 = B + static_cast<long>(j+3) * ldb;
+            const float* b4 = B + static_cast<long>(j+4) * ldb;
+            const float* b5 = B + static_cast<long>(j+5) * ldb;
+            const float* b6 = B + static_cast<long>(j+6) * ldb;
+            const float* b7 = B + static_cast<long>(j+7) * ldb;
             
-            double temp[4];
+            float temp[8];
             for (int t = 0; t < 4; ++t) {
-                // std::cout << i << " " << j << " " << t << std::endl;
-                __m256d acc = _mm256_setzero_pd();
+                __m256 acc = _mm256_setzero_ps();
                 for (int p = 0; p < K; ++p) {
-                    double Aa = (double)a[t][p];
-                    acc = _mm256_add_pd(acc, 
-                            _mm256_mul_pd(
-                                _mm256_set_pd(Aa, Aa, Aa, Aa),
-                                _mm256_set_pd((double)b0[p], (double)b1[p], (double)b2[p], (double)b3[p])
+                    float Aa = a[t][p];
+                    acc = _mm256_add_ps(acc, 
+                            _mm256_mul_ps(
+                                _mm256_set_ps(Aa, Aa, Aa, Aa, Aa, Aa, Aa, Aa),
+                                _mm256_set_ps((float)b0[p], (float)b1[p], (float)b2[p], (float)b3[p], (float)b4[p], (float)b5[p], (float)b6[p], (float)b7[p])
                             ));
                 }
-                _mm256_storeu_pd(temp, acc);
-                // first number is at highest position (3), weird
-                C[static_cast<long>(i+t) * ldc + j] = (float)temp[3];
-                C[static_cast<long>(i+t) * ldc + j+1] = (float)temp[2];
-                C[static_cast<long>(i+t) * ldc + j+2] = (float)temp[1];
-                C[static_cast<long>(i+t) * ldc + j+3] = (float)temp[0];
+                _mm256_storeu_ps(temp, acc);
+                C[static_cast<long>(i+t) * ldc + j] = (float)temp[7];
+                C[static_cast<long>(i+t) * ldc + j+1] = (float)temp[6];
+                C[static_cast<long>(i+t) * ldc + j+2] = (float)temp[5];
+                C[static_cast<long>(i+t) * ldc + j+3] = (float)temp[4];
+                C[static_cast<long>(i+t) * ldc + j+4] = (float)temp[3];
+                C[static_cast<long>(i+t) * ldc + j+5] = (float)temp[2];
+                C[static_cast<long>(i+t) * ldc + j+6] = (float)temp[1];
+                C[static_cast<long>(i+t) * ldc + j+7] = (float)temp[0];
             }
         }
     }
